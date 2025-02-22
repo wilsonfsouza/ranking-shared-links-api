7bd230ef8894cf0f738e6ec90d0fe14f65cf5183
Wilson Franca <wilson.franca.92@gmail.com>
2025-02-22 14:39:59 -0800

    feat: update gitignore and check if user is already subscribed

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -1,3 +1,4 @@
+import { eq } from 'drizzle-orm'
 import { db } from '../drizzle/client'
 import { subscriptions } from '../drizzle/schema/subscriptions'
 
@@ -10,6 +11,15 @@ export async function subscribeToEvent({
   name,
   email,
 }: SubscribeToEventParams) {
+  const subscribers = await db
+    .select()
+    .from(subscriptions)
+    .where(eq(subscriptions.email, email))
+
+  if (subscribers.length > 0) {
+    return { subscriberId: subscribers[0].id }
+  }
+
   const result = await db
     .insert(subscriptions)
     .values({
